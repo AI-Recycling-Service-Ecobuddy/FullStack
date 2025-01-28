b71b29b467786827e4ea08e2a6303b19b0343267
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-29 02:21:06 +0900

    feat: implement getting trashcan infos in seogu area with API

--- app/api/map/openapi/route.ts
+++ app/api/map/openapi/route.ts
@@ -8,8 +8,7 @@ export async function GET() {
       'https://api.odcloud.kr/api/15127636/v1/uddi:b8d629cb-49a1-4dc7-aec8-924057cb0738',
       {
         headers: {
-          Authorization:
-            'Infuser b6x0DxC2SjTFTFaQBXbUH7xTPTlw+Lfhil1mSnq8zR3rgSnqFnUdoBQd4MKygy+Eeb87GcaCzIsS3hTkJnY1kw==',
+          Authorization: `Infuser ${process.env.OPEN_API_DECODE_KEY}`,
         },
       },
     );
@@ -19,8 +18,17 @@ export async function GET() {
       'https://api.odcloud.kr/api/15086885/v1/uddi:37782167-d720-4858-af83-aa92600d370f',
       {
         headers: {
-          Authorization:
-            'Infuser b6x0DxC2SjTFTFaQBXbUH7xTPTlw+Lfhil1mSnq8zR3rgSnqFnUdoBQd4MKygy+Eeb87GcaCzIsS3hTkJnY1kw==',
+          Authorization: `Infuser ${process.env.OPEN_API_DECODE_KEY}`,
+        },
+      },
+    );
+
+    // 서구 데이터 가져오기
+    const seoGuResponse = await axios.get(
+      'https://api.odcloud.kr/api/15041577/v1/uddi:cb541318-d90b-4bb3-be27-db9c32fa47ec',
+      {
+        headers: {
+          Authorization: `Infuser ${process.env.OPEN_API_DECODE_KEY}`,
         },
       },
     );
@@ -33,15 +41,19 @@ export async function GET() {
     }));
 
     const jungGuMarkers = jungGuResponse.data.data.map((item: any) => ({
-      address: `대구광역시 ${item['도로명 주소']}`,
-      location: item['행정동'],
+      address: item['주소'],
+      location: item['위 치'],
       title: '중구 쓰레기통',
     }));
 
-    // 수성구와 중구 마커 데이터를 병합
-    const markers = [...suseongGuMarkers, ...jungGuMarkers];
+    const seoGuMarkers = seoGuResponse.data.data.map((item: any) => ({
+      address: item['설치위치'],
+      location: item['설치지점'],
+      title: '서구 쓰레기통',
+    }));
 
-    // JSON 응답 반환
+    const markers = [...suseongGuMarkers, ...jungGuMarkers, ...seoGuMarkers];
+
     return NextResponse.json(markers);
   } catch (error) {
     console.error('Error fetching data:', error);
