3de93bf436bd1488b8f00cea41fe3bf57dfd3354
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-29 15:48:33 +0900

    feat: add defaultOptions in QueryClient

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -8,7 +8,18 @@ export default function QueryProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5, // 5분 동안 데이터가 유효
+            refetchOnWindowFocus: false, // 창이 다시 활성화될 때 자동 리패치 비활성화
+            retry: 2, // 요청 실패 시 2번까지 재시도
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
