5e80e4b5b6e0feab0600f11648c0c4a3e5afff09
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-29 12:44:12 +0900

    chore: change marker directory name

--- src/features/map/index.ts
+++ src/features/map/index.ts
@@ -1,6 +1,5 @@
 export { default as NaverMap } from './ui/NaverMap';
-export { default as NewMarkerForm } from './ui/NaverMap';
-export { getMarkers } from './api/getMarkers';
+export { default as NewMarkerForm } from './ui/NewMarkerForm';
 export { postMarker } from './api/postMarker';
 export * from './model/marker';
 export * from './model/types';
