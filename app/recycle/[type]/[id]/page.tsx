5338238298af35a3a4d9655b1924648058c1b8b5
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-31 23:19:41 +0900

    refactor: adjust responsive ui for recycle detail page

--- app/recycle/[type]/[id]/page.tsx
+++ app/recycle/[type]/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useRecycleStore } from '@/src/features/recycle/model/recycleDetailStore';
 import Image from 'next/image';
 
@@ -6,7 +7,7 @@ export default function DetailPages() {
   const { title, type, imgUrl, context, subcontext } = useRecycleStore();
 
   return (
-    <div className='mt-[100px] flex items-center justify-center p-4 md:mt-[200px]'>
+    <div className='relative flex h-full w-full items-center justify-center p-4'>
       <Image
         src='/nature.webp'
         fill
@@ -15,28 +16,33 @@ export default function DetailPages() {
         className='object-cover'
       />
 
-      <div className='z-10 w-full max-w-2xl overflow-hidden rounded-lg bg-white shadow-xl'>
-        <div className='relative h-64'>
+      <div className='relative z-10 w-2/3 overflow-hidden rounded-lg bg-white shadow-lg'>
+        <div className='relative h-96 lg:h-[550px]'>
           {imgUrl && (
             <Image src={imgUrl} alt={title} fill className='object-cover' />
           )}
-          <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50'>
-            <h1 className='text-center text-4xl font-bold text-white'>
-              {title}
-            </h1>
+          <div className='absolute inset-0 flex items-center justify-center'>
+            <div className='rounded-md bg-black/45 px-4 py-2'>
+              <h1 className='text-center text-4xl font-bold text-white'>
+                {title}
+              </h1>
+            </div>
           </div>
         </div>
+
         <div className='p-6'>
           <h2 className='mb-4 text-2xl font-semibold text-gray-800'>
             재활용 정보
           </h2>
-          <div>{context}</div>
-          <div className='space-y-4'>
+          <p className='text-gray-700'>{context}</p>
+
+          <div className='mt-4 space-y-4'>
             <div className='flex items-center'>
               <span className='w-24 font-medium text-green-600'>분류:</span>
               <span className='text-gray-700'>{type}</span>
             </div>
           </div>
+
           <div className='mt-6 border-t border-gray-200 pt-6'>
             <h3 className='mb-2 text-lg font-semibold text-gray-800'>
               재활용 팁
