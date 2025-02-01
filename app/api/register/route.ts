5ad68c71001e6538e1e00034939a4eea925bca2d
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-01 16:02:13 +0900

    refactor: add logic to check existing User in register route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,12 +9,22 @@ export async function POST(req: NextRequest) {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await connectMongoDB();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: 'Email already exists.' },
+        { status: 400 },
+      );
+    }
+
     await User.create({ name, email, password: hashedPassword });
 
     return NextResponse.json({ message: 'User registered.' }, { status: 201 });
   } catch (error) {
+    console.error('Registration error:', error);
     return NextResponse.json(
-      { message: 'An error occured while registering the user.' },
+      { message: 'An error occurred while registering the user.', error },
       { status: 500 },
     );
   }
